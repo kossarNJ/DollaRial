3f726d7441a76204cba5f7cf5b5c178ed4b13b93
Soroush Ebadian <soroushebadian@gmail.com>
2018-04-16 17:59:44 +0430

    Add models to seleniumtest by django tutorial

--- dollarial/seleniumtest/views.py
+++ dollarial/seleniumtest/views.py
@@ -1,3 +1,22 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from seleniumtest.models import Question
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    return HttpResponse("You're looking at the results of question %s." % question_id)
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
