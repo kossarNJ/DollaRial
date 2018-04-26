53acc1f761c7a50d96e89a4487da900daa970278
Soroush Ebadian <soroushebadian@gmail.com>
2018-04-26 14:07:34 +0430

    Add selenium test

--- dollarial/seleniumtest/tests.py
+++ dollarial/seleniumtest/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.webdriver.firefox.webdriver import WebDriver
 
-# Create your tests here.
+from seleniumtest.models import Question
+
+
+class MySeleniumTests(StaticLiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.selenium = WebDriver()
+        cls.selenium.implicitly_wait(10)
+
+        q0 = Question.objects.create(question_text='halet chetore?')
+        q1 = Question.objects.create(question_text='hava khube?')
+        q0.save()
+        q1.save()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+
+    def test_index(self):
+        self.selenium.get('%s%s' % (self.live_server_url, '/seleniumtest/'))
+        print("address:", self.live_server_url)
+        question0 = self.selenium.find_element_by_name("question_1")
+        question1 = self.selenium.find_element_by_name("question_2")
+        self.selenium.find_element_by_xpath('//li[@name="question_1"]/a').click()
