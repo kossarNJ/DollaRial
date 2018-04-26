034245d314c75bea2616e4eae56adfcf49181e2c
Soroush Ebadian <soroushebadian@gmail.com>
2018-04-26 11:00:26 +0430

    Add django templates for vote selenium test
    add pip requirements

--- dollarial/seleniumtest/views.py
+++ dollarial/seleniumtest/views.py
@@ -1,11 +1,16 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
+from django.template import loader
 
 from seleniumtest.models import Question
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'detail.html', {'question': question})
 
 
 def results(request, question_id):
@@ -13,9 +18,10 @@ def results(request, question_id):
 
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    latest_questions = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('index.html')
+    context = {'latest_questions': latest_questions}
+    return HttpResponse(template.render(context, request))
 
 
 def vote(request, question_id):
