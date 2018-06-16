94da1ea6f884c505789df4ce187caa39a550a13b
Parand Alizadeh <parand1997@gmail.com>
2018-06-16 23:42:00 +0430

    add services test

--- dollarial/website/tests/test_services.py
+++ dollarial/website/tests/test_services.py
@@ -21,17 +21,58 @@ class ServicesTestCase(StaticLiveServerTestCase):
         return element.get_attribute('textContent')
 
 
-    def test_history_text(self):
+    def __add_data(self):
+
+        #todo add to db
+        data = [
+                {
+                    "name": "Toefl",
+                    "type": "exam",
+                    "price": "200$",
+                    "details": "Toefl Exam",
+
+                },
+                {
+                    "name": "University Payment",
+                    "type": "University",
+                    "price": "unknown",
+                    "details": "University Fee",
+
+                },
+                {
+                    "name": "Transfer Money to Foreign Account",
+                    "type": "transfer",
+                    "price": "unknown",
+                    "details": "transfer to another countries",
+
+                },
+                {
+                    "name": "Transfer Money to Internal Account",
+                    "type": "transfer",
+                    "price": "unknown",
+                    "details": "transfer to Iran",
+
+                },
+
+            ]
+        return data
+
+
+    def test_services(self):
+        data = self.__add_data()
         self.selenium.get('%s%s' % (self.live_server_url, '/services/'))
-        history = self.selenium.find_element_by_id("history_text")
-        self.assertIn("Something about us.", self.__get_text(history))
+        i = 0
+        for d in data:
+            name = self.selenium.find_element_by_id("name_" + str(i))
+            t = self.selenium.find_element_by_id("type_" + str(i))
+            price = self.selenium.find_element_by_id("price_"+ str(i))
+            details = self.selenium.find_element_by_id("details_" + str(i))
+            self.assertIn(d["name"], self.__get_text(name))
+            self.assertIn(d["type"], self.__get_text(t))
+            self.assertIn(d["price"], self.__get_text(price))
+            self.assertIn(d["details"], self.__get_text(details))
+            i = i + 1
 
 
-    def test_history_contact_details(self):
-        self.selenium.get('%s%s' % (self.live_server_url, '/history/'))
-        address = self.selenium.find_element_by_id("dollarial_address")
-        phone = self.selenium.find_element_by_id("dollarial_phone")
-        email = self.selenium.find_element_by_id("dollarial_email")
-        self.assertIn("Department of Computer Enginerring, Sharif University of Technology", self.__get_text(address))
-        self.assertIn("21 6600 5616", self.__get_text(phone))
-        self.assertIn("dollarialsharif@gmail.com", self.__get_text(email))
+
+#TODO click on each service?
