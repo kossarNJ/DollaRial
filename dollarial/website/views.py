95e0527e87df2b73fac3a2fe985a7caf50bdb7df
kossarNJ <kossarna@gmail.com>
2018-08-22 13:53:39 +0430

    Add cover pictures to website. Finish website TODO: change color of top menu in login and sign up page.

--- dollarial/website/views.py
+++ dollarial/website/views.py
@@ -14,13 +14,13 @@ def contact(request):
     else:
         form = ContactForm(request.POST)
         if form.is_valid():
-
             subject = form.cleaned_data['subject']
             email = form.cleaned_data['email']
             message = form.cleaned_data['message']
             name = form.cleaned_data['name']
 
-            sg = sendgrid.SendGridAPIClient(apikey='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')
+            sg = sendgrid.SendGridAPIClient(
+                apikey='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')
             from_email = Email(email)
             to_email = Email("parand1997@gmail.com")
             subject = subject
@@ -74,41 +74,3 @@ def currencies(request):
         ]
     }
     return render(request, 'website/currencies.html', currency_data)
-
-
-def services(request):
-    # TODO: read from db
-    data = {
-        "typeforms": [
-            {
-                "name": "Toefl",
-                "type": "exam",
-                "price": "200$",
-                "details": "Toefl Exam",
-
-            },
-            {
-                "name": "University Payment",
-                "type": "University",
-                "price": "unknown",
-                "details": "University Fee",
-
-            },
-            {
-                "name": "Transfer Money to Foreign Account",
-                "type": "transfer",
-                "price": "unknown",
-                "details": "transfer to another countries",
-
-            },
-            {
-                "name": "Transfer Money to Internal Account",
-                "type": "transfer",
-                "price": "unknown",
-                "details": "transfer to Iran",
-
-            },
-
-        ]
-    }
-    return render(request, 'website/services.html', data)
